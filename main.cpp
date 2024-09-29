8afb449ff6aee4ec60e0714c983fa756ee9f110c
Bacchus Jackson <34331871+BacchusJackson@users.noreply.github.com>
2024-09-29 13:54:47 -0500

    add problem 1

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "My Counting App" << std::endl;
+  std::string input = "abc.def.ghi.jkl";
+
+  std::cout << "My Input: " << input << std::endl;
+
+  int delimiterCount = 0;
+
+  // INSERT WORK HERE
+
+  std::cout << "My Output: " << delimiterCount << std::endl;
+
+  return 0;
+}
